77bf425f409f53bf3c1f117cbc9725a2584d2caf
Valentyna Gorbachenko <vgorbachenko@apple.com>
2017-09-01 17:47:11 -0700

    Uodate GetSum class

--- src/testJava/GetSum.java
+++ src/testJava/GetSum.java
@@ -7,6 +7,23 @@ public class GetSum {
 		getSum(a,b);
 	}
 	public static int getSum(int a, int b) {
+		int res = 0;
+		
+		if (a != b) {
+			for(int i=0; a>b || b>a; i++) {
+				if((a>>i)&1 == 0 || (b>>i)&1 == 0) {
+					
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	/*
+	public static int getSum(int a, int b) {
 		int num = a*2;
 		if (a != b) {
 			num = bitSum(Integer.toBinaryString(a), Integer.toBinaryString(b));
@@ -22,9 +39,7 @@ public class GetSum {
 	}
 	public static String addBits(String a, String b) {
 		String x = "";
-//		if ( a == "1" && b == "1") {
-//			
-//		}
 		return x;
 	}
+	*/
 }
